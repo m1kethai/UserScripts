9efcc8c0ee16fc647a72f6275e620d75115e6835
Mike Thai <15115669+m1kethai@users.noreply.github.com>
2024-08-27 17:28:26 -0400

    nixpkg stargazers: move script to new sub-project

---
+++ nixos-pkg-stargazers.user.js
@@ -0,0 +1,110 @@
+// ==UserScript==
+// @name         NixOS Package Search - GitHub repo stargazers badges for package search results
+// @namespace    https://github.com/m1kethai/UserScripts
+// @supportURL   https://github.com/m1kethai/UserScripts
+// @version      1.2
+// @description  Adds a badge displaying the number of GitHub repo stars for every Nix package (with a GitHub repo "Homepage") returned in the package search results.
+// @author       m1kethai
+// @match        https://search.nixos.org/packages*query*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nixos.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const styles = {
+        badgeEl: `
+            display: inherit;
+            margin-left: 1em;
+            padding: 0.04em 0.6em 0.04em 0.4em;
+            background-color: rgba(255, 255, 255, 0.15);
+            border-radius: 4px;
+            font-size: 0.8em;
+            font-weight: bold;
+            text-align: center;
+            transition: all 0.2s;
+        `,
+        badgeText: `
+            color: white;
+            text-decoration: none !important;
+            transition: all 0.2s;
+        `
+    };
+
+    async function pkgsWithGhRepoHomepages() {
+        const homepageLinkSelector = `div.search-page.success > div.search-results > div > ul > li.package > ul > li > a`;
+        const homepageLinks = document.querySelectorAll(homepageLinkSelector);
+        const githubRepoHomepages = Array.from(homepageLinks).filter(link => link.innerText.includes("Homepage") && link.href.includes("github.com") && !link.href.includes("blob"));
+
+        console.info(`🚀 ~ githubRepoHomepages:`, githubRepoHomepages)
+        return githubRepoHomepages;
+    }
+
+    async function fetchGithubRepoStars(ghRepoLink) {
+        try {
+            const
+                repoUrl = ghRepoLink.href,
+                apiUrl = new URL(`https://api.github.com/repos${repoUrl.replace("https://github.com", "")}`),
+                response = await fetch(apiUrl),
+                data = await response.json(),
+                gazers = data.stargazers_count;
+
+            return `⭐️ ${gazers || "???"}`;
+        } catch (error) {
+            console.error("Failed to fetch stars:", error);
+            return `⭐️ ???`;
+        }
+    }
+
+    const createBadgeElements = repoLinkList => repoLinkList.map(repoLink => {
+        const starsBadge = document.createElement("li"), starsLink = document.createElement("a");
+        starsBadge.appendChild(starsLink);
+        starsBadge.style = styles.badgeEl;
+        starsLink.style = styles.badgeText;
+        starsLink.target = "_blank";
+        starsLink.href = repoLink.href;
+        return starsBadge;
+    });
+
+    async function main() {
+        const repoLinkList = await pkgsWithGhRepoHomepages();
+        if (repoLinkList.length === 0) {
+            console.warn("No GitHub repo homepages found.");
+            return;
+        }
+        const badgeElements = createBadgeElements(repoLinkList);
+        const starsList = await Promise.all(repoLinkList.map(async repoLink => await fetchGithubRepoStars(repoLink)));
+        badgeElements.map((badge, i) => {
+            badge.querySelector("a").innerText = starsList[i]
+        });
+        repoLinkList.forEach((repoLink, i) => repoLink.parentElement.appendChild(badgeElements[i]));
+    }
+
+    function runWhenLoaded() {
+        if (document.readyState === "complete") {
+            main();
+        } else {
+            window.addEventListener('load', main);
+        }
+    }
+
+    function retryUntilSuccess() {
+        let retryCount = 0;
+        const maxRetries = 5;
+        const delay = 1000; // 1 second
+
+        const interval = setInterval(() => {
+            if (document.querySelector('div.search-page.success > div.search-results > div > ul > li.package')) {
+                clearInterval(interval);
+                runWhenLoaded();
+            } else if (retryCount >= maxRetries) {
+                clearInterval(interval);
+                console.warn("Max retries reached. Custom elements may not load properly.");
+            } else {
+                retryCount++;
+            }
+        }, delay);
+    }
+
+    retryUntilSuccess();
+})();
